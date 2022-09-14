d0180205338fe006ede6339aa8c8e45d6e21a180
MayaroKhan <110514239+MayaroKhan@users.noreply.github.com>
2022-09-14 18:30:29 +1000

    first commit

---
+++ task1.js
@@ -0,0 +1,19 @@
+var helloworld = 'HelloWorld!';
+console.log(helloworld);
+console.log(helloworld.charAt(5));
+
+let a = 'My name is';
+let b = 'Mayaro';
+
+console.log(a + b);
+console.log(a + b + '!');
+
+let c = 'mayarokhan';
+let d = 'HI'
+
+console.log(c.toUpperCase);
+console.log(c.toUpperCase + d.toLowerCase)
+
+
+
+
---
+++ task2.js
@@ -0,0 +1,14 @@
+let countries = ['Australia', 'Canada', 'Brazil'];
+let student = ['Vihan', 'Sam', 'Senh'];
+let number = [9, 3, 5, 7, 1];
+
+console.log(countries);
+console.log(countries.push('UK'));
+console.log(countries[0]);
+console.log(student.pop());
+console.log(number.sort());
+console.log(number.slice(1,3));
+console.log(student.slice(-2));
+
+
+
